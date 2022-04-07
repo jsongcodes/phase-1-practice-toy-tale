68b0e2b5a034e91710e676e27fdfa8b13bd8e2d4
Jasmin <ssongnari@gmail.com>
2022-04-06 21:23:40 -0400

    commit tree ee316437932da0e8b51e12a5b12bc67bac818254

--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getToys()
   const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  const toyForm = document.querySelector(".container")
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyForm.style.display = "block";
     } else {
-      toyFormContainer.style.display = "none";
+      toyForm.style.display = "none";
     }
   });
+  toyForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    postToy(e.target.name.value, e.target.image.value)
+  })
 });
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+    .then((res) => { res.json() })
+    .then(function (data) {
+      data.map(t => renderToy(t))
+      //console.log(data)
+    })
+}
+
+function renderToy(toy) {
+
+  const toyCard = `< div class="card" >
+<h2>${toy.name}</h2>
+<img src="${toy.image}" class="toy-avatar" />
+<p>${toy.likes} Likes</p>
+<button class="like-btn" id="[toy_id]">Like ❤️</button>
+</div >`
+  const toyBox = document.getElementById('toy-collection')
+  toyBox.innerHTML += toyCard
+}
+
+function postToy(name, url) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "image": url,
+      "likes": 0
+    })
+
+  })
+    .then(function (response) {
+      return response.json();
+
+    })
+    .then(function (data) {
+      renderToy(data)
+    })
+}
